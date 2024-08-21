1c219e6965fc28d5fdde5da57c7d1390bdefbd7a
vaproh <vaproh@proton.me>
2024-08-21 15:39:43 +0530

    fixed firefox icon

--- config.def.h
+++ config.def.h
@@ -126,7 +126,7 @@ static Sp scratchpads[] = {
  */
 static char *tagicons[][NUMTAGS] =
 {
-	[DEFAULT_TAGS]        = { "󰣇", "", "", "", "󰚌", "", "󰕧", "󰻞", "󰺷" },
+	[DEFAULT_TAGS]        = { "󰣇", "", "󰈹", "", "󰚌", "", "󰕧", "󰻞", "󰺷" },
 	[ALTERNATIVE_TAGS]    = { "A", "B", "C", "D", "E", "F", "G", "H", "I" },
 	[ALT_TAGS_DECORATION] = { "<1>", "<2>", "<3>", "<4>", "<5>", "<6>", "<7>", "<8>", "<9>" },
 };
